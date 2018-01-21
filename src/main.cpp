b8bdb65f245184e96647a1bec25dea75df09a410
Lübbe <luebbe.opensource@gmail.com>
2018-01-21 13:44:49 +0100

    Since serial logging is up and running, switch to my ota handler

--- src/main.cpp
+++ src/main.cpp
@@ -4,32 +4,33 @@
 #include <Homie.h>
 #include <ArduinoOTA.h>
 
+#include "ota.hpp"
 #include "welcome.cpp"
 #include "H801Node.hpp"
 
+OtaLogger ota;
 Welcome welcome(FW_NAME, FW_VERSION);
 
 H801Node h801node("h801");
 
 void setupHandler()
 {
-  ArduinoOTA.setHostname(Homie.getConfiguration().deviceId);
-  ArduinoOTA.begin();
+  ota.setup();
 }
 
 void loopHandler()
 {
-  ArduinoOTA.handle();
+  ota.loop();
 }
 
 void setup()
 {
   Homie_setFirmware(FW_NAME, FW_VERSION);
 
-  // 
+  // !!!Attention!!!
+  // The H801 controller uses serial1 for output
   Serial1.begin(SERIAL_SPEED);
   Homie.setLoggingPrinter(&Serial1);
-  Homie.setLedPin(LED_PIN_RED, HIGH);
 
   welcome.show();
 
